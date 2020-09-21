09f809f648c936388117414af58865b1a28097c1
Christian Oellers <info@codeconut.io>
2020-09-21 16:54:15 +0200

    Fix prettier

--- templates/tampermonkey-userscript.js
+++ templates/tampermonkey-userscript.js
@@ -2,12 +2,12 @@
 // @name         Spellcheck Website Content Helper
 // @description  Enables browser plugin spell checking abilities within website content.
 // @author       Codeconut Ltd.
-// @version      //VERSION//
+// @version      #VERSION#
 // @grant        none
 // @namespace    Codeconut_SpellCheckHelper
 // @include      http*://dev-*.test*
 // @include      http*://localhost*
 // ==/UserScript==
 (() => {
-  // SCRIPT//
+  #SCRIPT#
 })();
