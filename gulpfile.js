fcab048468f7e520e9c35a87b71f61add6729966
Christian Oellers <info@codeconut.io>
2020-09-23 14:57:33 +0200

    Gulp + ESlint

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,25 @@
 /* eslint-disable no-console */
-const gulp = require('gulp');
-const gulpPromiseAll = require('gulp-all');
-const replace = require('gulp-replace');
-const { src, dest } = require('gulp');
+const gulp = require("gulp");
+const gulpPromiseAll = require("gulp-all");
+const replace = require("gulp-replace");
+const { src, dest } = require("gulp");
 
-const copyFiles = () => src('src/*.js').pipe(dest('dist/'));
+const copyFiles = () => src("src/*.js").pipe(dest("dist/"));
 
-gulp.task('result', () => {
-  gulp.src(['templates/tampermonkey-userscript.js']).pipe(replace('#SCRIPT#', 'XXX')).pipe(gulp.dest('build/'));
+gulp.task("result", () => {
+  gulp
+    .src(["templates/tampermonkey-userscript.js"])
+    .pipe(replace("#SCRIPT#", "XXX"))
+    .pipe(gulp.dest("build/"));
 });
 
-gulp.task('default', () => gulpPromiseAll(copyFiles()).then(
-  () => {
-    console.log('SUCCESS');
-  },
-  (err) => {
-    console.error('ERROR:', err);
-  },
-));
+gulp.task("default", () =>
+  gulpPromiseAll(copyFiles()).then(
+    () => {
+      console.log("SUCCESS");
+    },
+    (err) => {
+      console.error("ERROR:", err);
+    }
+  )
+);
